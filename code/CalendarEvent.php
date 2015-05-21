nyeholt/silverstripe-event-calendar

commit 9b2a38f6510bba6a97308e8bdde72724439f3331

Add new field for event Location.

code/CalendarEvent.php
@@ -3,6 +3,7 @@
 class CalendarEvent extends Page {
 	
 	private static $db = array (
+		'Location' => 'Text',
 		'Recursion' => 'Boolean',
 		'CustomRecursionType' => 'Int',
 		'DailyInterval' => 'Int',
@@ -38,6 +39,13 @@ class CalendarEvent extends Page {
 		
 		$this->beforeUpdateCMSFields(function($f) use ($self) {
 			Requirements::javascript('event_calendar/javascript/calendar_cms.js');
+			
+			$f->addFieldToTab("Root.Main",
+				TextField::create(
+					"Location",
+					_t('Calendar.LOCATIONDESCRIPTION','The location for this event'),
+				), 'Content')
+			);
 			
 			$dt = _t('CalendarEvent.DATESANDTIMES','Dates and Times');
 			$recursion = _t('CalendarEvent.RECURSION','Recursion');

commit 8e5e464e9702c933fcd2a882c2c8a009edbfe2d2

FIX parse error from too many parenthesis
Introduced in https://github.com/unclecheese/silverstripe-event-calendar/commit/9b2a38f6510bba6a97308e8bdde72724439f3331#diff-303cb0f05b850374829e7f607f9ef022R47

code/CalendarEvent.php
@@ -44,7 +44,8 @@ class CalendarEvent extends Page {
 				TextField::create(
 					"Location",
 					_t('Calendar.LOCATIONDESCRIPTION','The location for this event'),
-				), 'Content')
+				), 
+				'Content'
 			);
 			
 			$dt = _t('CalendarEvent.DATESANDTIMES','Dates and Times');